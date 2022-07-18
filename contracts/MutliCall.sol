2cb2800e44a0d3a4db6609e70010059417213d5f
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-07-18 22:06:15 +0200

    Initialize

--- contracts/MutliCall.sol
+++ contracts/MutliCall.sol
@@ -2,7 +2,20 @@
 
 pragma solidity ^0.8.0;
 
+error LengthNotEqual();
+error CallFailed();
+
 contract MultiCall {
 
-
+    function mutliCall(address[] calldata targets, bytes[] calldata data) external view returns (bytes[] memory) {
+        if (targets.length != data.length) revert LengthNotEqual();
+        bytes[] memory results = new bytes[](data.length);
+        for (uint256 i; i < targets.length;) {
+            (bool success, bytes memory result) = targets[i].staticcall(data[i]);
+            if (!success) revert CallFailed();
+            results[i] = result;
+            unchecked {i++;}
+        }
+        return results;
+    }
 }
